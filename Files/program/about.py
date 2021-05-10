c5c17cea947f9b7db93c4a2c192ef0c7a7c4e1fc
zentkhv <70746340+zentkhv@users.noreply.github.com>
2021-05-10 10:47:08 +0300

    +about

---
+++ Files/program/about.py
@@ -0,0 +1,13 @@
+def text_author():
+    data = "Автор: Ильченко Михаил Александрович\nГруппа: СО251КОБ\n"
+    return data
+
+
+def text_about():
+    data = "Вконтакте: https://vk.com/mikhail27rus\nЯзык программирования: Python"
+    return data
+
+
+def text_program():
+    data = "Добро пожаловать в Securegram!\n\nЭта программа предназначена для безопасного обмена сообщениями."
+    return data
